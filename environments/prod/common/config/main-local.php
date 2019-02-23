86134cdefd637d6aa576279fa9148b77b4e2a903
陈凯 <ckhero@chenkaideMacBook-Pro.local>
2019-02-23 16:17:23 +0800

    mysql config

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhsot;dbname=guava',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
