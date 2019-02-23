dbd3134c98398b1cc84c1b617b201df9a395f439
陈凯 <ckhero@chenkaideMacBook-Pro.local>
2019-02-24 02:26:45 +0800

    fix

--- common/services/UserService.php
+++ common/services/UserService.php
@@ -9,12 +9,15 @@
 namespace common\services;
 
 
+use api\events\LoginEvent;
 use common\components\Log;
 use common\consts\ErrorConst;
+use common\consts\EventConst;
 use common\consts\LogTypeConst;
 use common\consts\SystemConst;
 use common\exceptions\DefaultException;
 use common\models\User;
+use common\models\UserToken;
 use EasyWeChat\Factory;
 use Yii;
 
@@ -25,8 +28,7 @@ class UserService
      * @param string $iv
      * @param string $encryptData
      * @return array
-     * @throws \common\exceptions\DefaultException
-     * @throws \yii\base\Exception
+     * @throws DefaultException
      */
     public function login(string $code, string $iv, string $encryptData)
     {
@@ -34,13 +36,12 @@ class UserService
             $app = Factory::miniProgram(Yii::$app->params[SystemConst::PARAMS_CONFIG_MINI_PROGRAM]);
             $sessionInfo = $app->auth->session($code);
             $baseInfo = $app->encryptor->decryptData($sessionInfo['session_key'], $iv, $encryptData);
-            return $baseInfo;
-//            $user = (new User())->findOrCreate($openid);
-//            $userToken = (new UserToken())->createOrderUpdate($user->user_id);
-//
-//            Yii::$app->trigger(EventConst::EVENT_LOGIN, (new LoginEvent($user)));
+
+            $user = (new User())->findOrCreate($sessionInfo['openid'], $baseInfo['nickName'], '', $baseInfo['avatarUrl']);
+            $userToken = (new UserToken())->createOrderUpdate($user->user_id);
+
+            Yii::$app->trigger(EventConst::EVENT_LOGIN, (new LoginEvent($user)));
         } catch (\Exception $e) {
-            return $e->getMessage();
             Log::error(ErrorConst::msg(ErrorConst::ERROR_LOGIN_FAIL), [
                 func_get_args(),
                 'message' => $e->getMessage()
@@ -48,10 +49,10 @@ class UserService
             throw new DefaultException(ErrorConst::ERROR_LOGIN_FAIL);
         }
 
-//        return [
-//            'token' => $userToken->user_token_token,
-//            'expire_at' => $userToken->user_token_expire_at,
-//        ];
+        return [
+            'token' => $userToken->user_token_token,
+            'expire_at' => $userToken->user_token_expire_at,
+        ];
     }
 
     /**
