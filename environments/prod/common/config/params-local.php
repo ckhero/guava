6e6c5e169585e7fb2b2e64b282019350c3c0e901
陈凯 <ckhero@chenkaideMacBook-Pro.local>
2019-05-05 20:25:41 +0800

    fix

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'mini-program' => [
-        'app_id' => 'wxbdda40128fda63c7',
-        'secret' => 'bd9439f7e33977af93483ea06d9a8bba',
+        'app_id' => 'wxfc3abbaf412150d2',
+        'secret' => '4f1e6dc4b64bfbd9b11cb084a67c4e76',
 
         // 下面为可选项
         // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
