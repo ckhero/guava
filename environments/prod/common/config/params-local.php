860153893edd090d585da6c3f44a67c976c8dfaf
陈凯 <ckhero@chenkaideMacBook-Pro.local>
2019-06-18 23:26:10 +0800

    fix

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -5,7 +5,7 @@ return [
         'secret' => '4f1e6dc4b64bfbd9b11cb084a67c4e76',
         'mch_id' => '1533854671',
         'key' => 'b0baee9d279d34fa1dfd71aadb908c3f',
-        'notify_url' => 'https://guava.51qwer.com/v1/order/notify',
+        'notify_url' => 'https://www.goodexam.com.cn/v1/order/notify',
         // 下面为可选项
         // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
         'response_type' => 'array',
@@ -17,9 +17,9 @@ return [
     ],
     'uploads' => [
         'path' => '/data/www/datum/',
-        'url' => 'https://www.passexam.me/',
+        'url' => 'https://www.goodexam.com.cn/',
     ],
     'domain' => [
-        'site' => 'https://www.passexam.me/',
+        'site' => 'https://www.goodexam.com.cn/',
     ],
 ];
